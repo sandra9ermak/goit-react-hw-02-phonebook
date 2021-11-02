e41ff8ccc336daa0f39558e8d294d83eb86ecdfc
sandra9ermak <sandra1ermak@gmail.com>
2021-11-02 19:15:28 +0200

    fin

--- src/App/App.js
+++ src/App/App.js
@@ -8,12 +8,7 @@ import Filter from "../components/Form/Filter";
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -24,7 +19,11 @@ class App extends React.Component {
       number,
     };
 
-    if (this.state.contacts.some((item) => item.name === name)) {
+    if (
+      this.state.contacts.some(
+        (item) => item.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       return Notiflix.Notify.warning(`${name} is already in contacts`);
     } else {
       this.setState((prevState) => ({
